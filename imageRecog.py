7bb9c67a7692602a57d17c38127a4ba1c17b2e3e
Kytzis <inge@crazy4life.no>
2021-06-19 16:28:59 +0200

    Added automatic cropping in x-direction, and manual cropping in y-direction

--- imageRecog.py
+++ imageRecog.py
@@ -1,11 +1,12 @@
-def getData(directory):
-    import matplotlib.image as mpimg
-    import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
-    brightnessThreshold = .3
+def getData(directory):
 
     img = mpimg.imread(directory)
 
+    # Apply black/white filter for ease of read
+    brightnessThreshold = .25
     for row in img:
         for pixel in row:
             if ((pixel[0] + pixel[1] + pixel[2])/3 < brightnessThreshold):
@@ -17,7 +18,22 @@ def getData(directory):
                 pixel[1] = 1
                 pixel[2] = 1
 
-    plt.imshow(img)
+    # Crop down image to only the area with price (given by where parentheses are)
+    numStart = -1
+    for pixel in range(100, len(img[34])):
+        if img[34][pixel][0] == 1:
+            numStart = pixel
+            break
+    numEnd = -1
+    for pixel in range(len(img[34])-100, 0, -1):
+        if img[34][pixel][0] == 1:
+            numEnd = pixel+1
+            break
+
+    # Crop down, and offset for parentheses
+    cropped = img[16:42, numStart+2:numEnd-2]
+
+    plt.imshow(cropped)
     plt.show()
 
 

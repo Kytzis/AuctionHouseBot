bf7c3f0935885551704998a8538f6f85d0904b27
Kytzis <inge@crazy4life.no>
2021-06-19 15:43:11 +0200

    Test program for reading image

--- imageRecog.py
+++ imageRecog.py
@@ -1,1 +1,26 @@
-test = 1
+def getData(directory):
+    import matplotlib.image as mpimg
+    import matplotlib.pyplot as plt
+
+    brightnessThreshold = .3
+
+    img = mpimg.imread(directory)
+
+    for row in img:
+        for pixel in row:
+            if ((pixel[0] + pixel[1] + pixel[2])/3 < brightnessThreshold):
+                pixel[0] = 0
+                pixel[1] = 0
+                pixel[2] = 0
+            else:
+                pixel[0] = 1
+                pixel[1] = 1
+                pixel[2] = 1
+
+    plt.imshow(img)
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    getData('./test.png')
